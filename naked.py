262ed379cc88a46399e94a11a0297011c7441806
Susliki <enaglis@gmail.com>
2021-12-14 20:44:46 +0000

    nokonfigurets arejā lasisana no areja avota

--- naked.py
+++ naked.py
@@ -5,14 +5,23 @@ import time
 import yaml
 
 from datetime import datetime
+from configparser import ConfigParser
 print('Asteroid processing service')
 
 # Initiating and reading config values
 print('Loading configuration from file')
+try:
+                config = ConfigParser()
+                config.read('config.ini')
 
-# 
-nasa_api_key = "JPx41PVAZmdTxQHCOrKOqekryksO8N23sdRa8dn5"
-nasa_api_url = "https://api.nasa.gov/neo/"
+                nasa_api_key = config.get('nasa', 'api_key')
+                nasa_api_url = config.get('nasa', 'api_url')
+
+
+except:
+                logger.exception('')
+print('DONE')
+#
 
 # Getting todays date
 dt = datetime.now()
