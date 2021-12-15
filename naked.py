a398719126efa17e1c5d16b022f4a9176a335742
Edgars Naglis <76187459+susliki@users.noreply.github.com>
2021-12-16 00:49:54 +0200

    Update naked.py

--- naked.py
+++ naked.py
@@ -141,109 +141,109 @@ def sort_ast_by_time(ast_arr):
 if __name__ == "__main__":
 
 
-	connection = None
-	connected = False
+  connection = None
+  connected = False
 
-	init_db()
+  init_db()
 
 # Getting todays date
 #Dabūjam sodienas datumu lai varetu iaveidot korektu pierasiju pret nasa serveri
-	dt = datetime.now()
-	request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
-	logger.debug("Generated today's date: " + str(request_date))
+    dt = datetime.now()
+    request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
+    logger.debug("Generated today's date: " + str(request_date))
 
-#sini bloka aprakstitis ka tiek veikts pats pieprasijums un  padod lidzi  parametrus ka piem atslega un datums
-	  logger.debug("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
-	  r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
+  #sini bloka aprakstitis ka tiek veikts pats pieprasijums un  padod lidzi  parametrus ka piem atslega un datums
+      logger.debug("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
+      r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
-	  logger.debug("Response status code: " + str(r.status_code))
-	  logger.debug("Response headers: " + str(r.headers))
-	  logger.debug("Response content: " + str(r.text))
-#ja atgriez 200 kas ir veiksmiga  http pierpasjuma kods
-	  if r.status_code == 200:
-#parsejam datus no json formatu uz diviem masiviem par drosajiem un bistamajiem asteroidiem
-   	 json_data = json.loads(r.text)
+      logger.debug("Response status code: " + str(r.status_code))
+      logger.debug("Response headers: " + str(r.headers))
+      logger.debug("Response content: " + str(r.text))
+  #ja atgriez 200 kas ir veiksmiga  http pierpasjuma kods
+      if r.status_code == 200:
+  #parsejam datus no json formatu uz diviem masiviem par drosajiem un bistamajiem asteroidiem
+       json_data = json.loads(r.text)
 
-   	 ast_safe = []
-  	  ast_hazardous = []
-#parbaude vai tads elemts eksiste  padotajos datus
-   	 if 'element_count' in json_data:
-    	  ast_count = int(json_data['element_count'])
-     	 logger.info("Asteroid count today: " + str(ast_count))
-#ja ir vairak par nulli ejam talak un  nolasam datus no nasa iedotajiem datiem.( vards,url uz NASA lapu, MIN MAX izmeris, laiku kad pietuvosies
-    	  if ast_count > 0:
-       	 for val in json_data['near_earth_objects'][request_date]:
-        	  if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
-        	    tmp_ast_name = val['name']
-        	    tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
-        	    if 'kilometers' in val['estimated_diameter']:
-             	 if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
-              	  tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
-            	    tmp_ast_diam_max = round(val['estimated_diameter']['kilometers']['estimated_diameter_max'], 3)
-              else:
-                tmp_ast_diam_min = -2
-                tmp_ast_diam_max = -2
-            else:
-                tmp_ast_diam_min = -1
-              tmp_ast_diam_max = -1
-
-            tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
-
-            if len(val['close_approach_data']) > 0:
-              if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
-                tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
-                tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
-                tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
-
-                if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
-                  tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
+       ast_safe = []
+        ast_hazardous = []
+  #parbaude vai tads elemts eksiste  padotajos datus
+       if 'element_count' in json_data:
+          ast_count = int(json_data['element_count'])
+         logger.info("Asteroid count today: " + str(ast_count))
+  #ja ir vairak par nulli ejam talak un  nolasam datus no nasa iedotajiem datiem.( vards,url uz NASA lapu, MIN MAX izmeris, laiku kad pietuvosies
+          if ast_count > 0:
+           for val in json_data['near_earth_objects'][request_date]:
+              if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
+                tmp_ast_name = val['name']
+                tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
+                if 'kilometers' in val['estimated_diameter']:
+                 if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
+                    tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
+                    tmp_ast_diam_max = round(val['estimated_diameter']['kilometers']['estimated_diameter_max'], 3)
                 else:
-                  tmp_ast_speed = -1
-
-                if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
-                  tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
-                else:
-                  tmp_ast_miss_dist = -1
+                  tmp_ast_diam_min = -2
+                  tmp_ast_diam_max = -2
               else:
-                tmp_ast_close_appr_ts = -1
-                tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
-                tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
-            else:
-              logger.warning("No close approach data in message")
-              tmp_ast_close_appr_ts = 0
-              tmp_ast_close_appr_dt_utc = "1970-01-01 00:00:00"
-              tmp_ast_close_appr_dt = "1970-01-01 00:00:00"
-              tmp_ast_speed = -1
-              tmp_ast_miss_dist = -1
+                  tmp_ast_diam_min = -1
+                tmp_ast_diam_max = -1
 
-            logger.info("------------------------------------------------------- >>")
-            logger.info("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
-            logger.info("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
-            logger.info("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
-          #ja pec augstak minetajime pararmetriem atbilst tad ieliekam attiecigaja saraksta
-          # Adding asteroid data to the corresponding array
-            if tmp_ast_hazardous == True:
-              ast_hazardous.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
-            else:
-              ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
-#ja nav neviena tad iegustam sadu rezultatu ka neka sodien nebus
+              tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
+
+              if len(val['close_approach_data']) > 0:
+                if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
+                  tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
+                  tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
+                  tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
+
+                  if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
+                    tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
+                  else:
+                    tmp_ast_speed = -1
+
+                  if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
+                    tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
+                  else:
+                    tmp_ast_miss_dist = -1
+                else:
+                  tmp_ast_close_appr_ts = -1
+                  tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
+                  tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
+              else:
+                logger.warning("No close approach data in message")
+                tmp_ast_close_appr_ts = 0
+                tmp_ast_close_appr_dt_utc = "1970-01-01 00:00:00"
+                tmp_ast_close_appr_dt = "1970-01-01 00:00:00"
+                tmp_ast_speed = -1
+                tmp_ast_miss_dist = -1
+
+              logger.info("------------------------------------------------------- >>")
+              logger.info("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
+              logger.info("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
+              logger.info("Speed: " + str(tmp_ast_speed) + " km/h" + " | MISS distance: " + str(tmp_ast_miss_dist) + " km")
+            #ja pec augstak minetajime pararmetriem atbilst tad ieliekam attiecigaja saraksta
+            # Adding asteroid data to the corresponding array
+              if tmp_ast_hazardous == True:
+                ast_hazardous.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
+              else:
+                ast_safe.append([tmp_ast_name, tmp_ast_nasa_jpl_url, tmp_ast_diam_min, tmp_ast_diam_max, tmp_ast_close_appr_ts, tmp_ast_close_appr_dt_utc, tmp_ast_close_appr_dt, tmp_ast_speed, tmp_ast_miss_dist])
+  #ja nav neviena tad iegustam sadu rezultatu ka neka sodien nebus
+        else:
+          logger.info("No asteroids are going to hit earth today")
+
+      logger.info("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
+
+      if len(ast_hazardous) > 0:
+  #sakarto pec sarakststa, kuri  var ietriekties, kuri palidos garam vai ka nav vispar
+        ast_hazardous.sort(key = lambda x: x[4], reverse=False)
+
+        logger.info("Today's possible apocalypse (asteroid impact on earth) times:")
+        for asteroid in ast_hazardous:
+          logger.info(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
+
+        ast_hazardous.sort(key = lambda x: x[8], reverse=False)
+        logger.info("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
       else:
-        logger.info("No asteroids are going to hit earth today")
-
-    logger.info("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
-
-    if len(ast_hazardous) > 0:
-#sakarto pec sarakststa, kuri  var ietriekties, kuri palidos garam vai ka nav vispar
-      ast_hazardous.sort(key = lambda x: x[4], reverse=False)
-
-      logger.info("Today's possible apocalypse (asteroid impact on earth) times:")
-      for asteroid in ast_hazardous:
-        logger.info(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
-
-      ast_hazardous.sort(key = lambda x: x[8], reverse=False)
-      logger.info("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
+        logger.info("No asteroids close passing earth today")
+  #ja ir kada kluda nolasot no API izdot kludas pazinojumu
     else:
-      logger.info("No asteroids close passing earth today")
-#ja ir kada kluda nolasot no API izdot kludas pazinojumu
-  else:
-    logger.error("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
+      logger.error("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
